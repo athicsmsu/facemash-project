6817687e38fff83d1b4cdae1e586db6de199d2c8
athicsmsu <65011212145@msu.ac.th>
2024-03-01 00:14:19 +0700

    new commit

---
+++ src/app/services/api/post.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { Constants } from '../../config/constants';
+import { HttpClient } from '@angular/common/http';
+import { lastValueFrom } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostService {
+
+  constructor(private constants : Constants,private http:HttpClient) { }
+
+  public async getPosts(options?:any){
+    let url = '';
+    if(options){
+      const id = options;
+      url = this.constants.API_ENDPOINT + '/posts/' + id;
+    }else{
+      url = this.constants.API_ENDPOINT + '/posts';
+    }
+    const response = await lastValueFrom(this.http.get(url));
+    console.log(url);
+    return response as any[];
+  }
+}
