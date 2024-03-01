0c2f23694d6078a1e62efe4e73d4d748b0e125c1
athicsmsu <65011212145@msu.ac.th>
2024-03-02 01:43:09 +0700

    new

--- src/app/page/user/user.component.ts
+++ src/app/page/user/user.component.ts
@@ -1,17 +1,19 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { UserService } from '../../services/api/user.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss'
 })
 export class UserComponent {
   id:any;
   user : any;
+  Avatar : any;
   constructor(private route: ActivatedRoute,private userService : UserService){
 		this.route.queryParams.subscribe(params =>{
 			this.id = params['user'];
@@ -22,6 +24,7 @@ export class UserComponent {
   }
   async loadDataAsync (){
     this.user = await this.userService.getUser(this.id);
-    // console.log(this.user);
+    this.Avatar = this.user[0].Avatar;
+    console.log(this.user);
   }
 }
