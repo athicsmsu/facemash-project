52f8769b02cc8dfe5ea57406725ee56408144a0c
athicsmsu <65011212145@msu.ac.th>
2024-03-03 01:52:33 +0700

    new

--- src/app/page/main/main.component.ts
+++ src/app/page/main/main.component.ts
@@ -1,29 +1,41 @@
 import { Component } from '@angular/core';
 import { PostService } from '../../services/api/post.service';
 import { CommonModule } from '@angular/common';
-import { ActivatedRoute, Router, RouterLink, RouterOutlet } from '@angular/router';
+import {
+  ActivatedRoute,
+  Router,
+  RouterLink,
+  RouterOutlet,
+} from '@angular/router';
+import { Constants } from '../../config/constants';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [CommonModule,RouterLink, RouterOutlet],
+  imports: [CommonModule, RouterLink, RouterOutlet],
   templateUrl: './main.component.html',
-  styleUrl: './main.component.scss'
+  styleUrl: './main.component.scss',
 })
 export class MainComponent {
+  id: any;
+  image: any[] = [];
+  score: any[] = [];
+  image1: any;
+  image2: any;
+  score1: any;
+  score2: any;
 
-  id:any;
-  image : any[] = [];
-  score : any[] = [];
-  image1 : any;
-  image2 : any;
-  score1 : any;
-  score2 : any;
-
-  constructor(private postService : PostService,private router: Router,private route: ActivatedRoute){
-    this.route.queryParams.subscribe(params =>{
-			this.id = params['user'];
-		}); 
+  constructor(
+    private postService: PostService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private constants: Constants,
+    private http: HttpClient
+  ) {
+    this.route.queryParams.subscribe((params) => {
+      this.id = params['user'];
+    });
   }
 
   ngOnInit(): void {
@@ -37,7 +49,7 @@ export class MainComponent {
     this.loadDataAsync();
   }
 
-  async loadDataAsync (){
+  async loadDataAsync() {
     this.image = await this.postService.getPosts();
     this.image1 = this.image[getRandomIndex(this.image)];
     do {
@@ -49,38 +61,66 @@ export class MainComponent {
     this.score1 = this.score[0].total_score;
     this.score = await this.postService.getScore(this.image2.Pid);
     this.score2 = this.score[0].total_score;
-    if(this.score1 == null){
+    if (this.score1 == null) {
       this.score1 = 0;
     }
-    if(this.score2 == null){
+    if (this.score2 == null) {
       this.score2 = 0;
     }
   }
 
-  Vote(WinPid : Number,LosePid : Number) {
+  Vote(WinPid: Number, LosePid: Number, check: Number) {
+    const url = this.constants.API_ENDPOINT + `/vote`;
     const K = 32;
-    console.log("Vote");
-    console.log("Pid : "+WinPid);
-    console.log(this.score1);
-    console.log(this.score2);
-    const EA = 1/(1+(10**((2600-3000)/400)));
-    console.log(EA);
-    const RA = K*(1-EA);
-    const EB = 1/(1+(10**((3000-2600)/400)));
-    console.log(EB);
-    // const EB = 1/1+10 (this.score2-this.score1)/400;
-  }
-  VoteWin2(Pid : Number) {
-    const K = 32;
-    console.log("Vote");
-    console.log("Pid : "+Pid);
-    console.log(this.score1);
-    console.log(this.score2);
-    const EA = 1/(1+(10**((2600-3000)/400)));
-    console.log(EA);
-    const EB = 1/(1+(10**((3000-2600)/400)));
-    console.log(EB);
-    // const EB = 1/1+10 (this.score2-this.score1)/400;
+    console.log('WinPid : ' + WinPid);
+    console.log('LosePid : ' + LosePid);
+    console.log('Check : ' + check);
+    const EA = 1 / (1 + 10 ** ((this.score2 - this.score1) / 400));
+    const EB = 1 / (1 + 10 ** ((this.score1 - this.score2) / 400));
+
+    if (check == 1) {
+      //กรณี A ชนะ
+      const RA = K * (1 - EA);
+      console.log(RA);
+      const RB = K * (0 - EB);
+      console.log(RB);
+      this.http
+        .post(url + '/win', {
+          Pid: WinPid,
+          score: RA,
+        })
+        .subscribe((data: any) => {
+          console.log(data);
+        });
+      this.http
+        .post(url + '/lose', {
+          Pid: LosePid,
+          score: RB,
+        })
+        .subscribe((data: any) => {
+          console.log(data);
+        });
+    } else if (check == 2) {
+      //กรณี ฺB ชนะ
+      const RA = K * (0 - EA);
+      console.log(RA);
+      const RB = K * (1 - EB);
+      console.log(RB);
+      this.http.post(url + '/win', {
+          Pid: WinPid,
+          score: RB,
+        })
+        .subscribe((data: any) => {
+          console.log(data);
+        });
+      this.http.post(url + '/lose', {
+          Pid: LosePid,
+          score: RA,
+        })
+        .subscribe((data: any) => {
+          console.log(data);
+        });
+    }
   }
 }
 function getRandomIndex(array: any[]): number {
