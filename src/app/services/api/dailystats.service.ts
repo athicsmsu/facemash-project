f04095fc7556a15bdefe395b7695ead29683f4d7
athicsmsu <65011212145@msu.ac.th>
2024-03-12 00:05:22 +0700

    commit

---
+++ src/app/services/api/dailystats.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Constants } from '../../config/constants';
+import { HttpClient } from '@angular/common/http';
+import { lastValueFrom } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DailystatsService {
+
+  constructor(private constants : Constants,private http:HttpClient) { }
+
+  public async getAllDailystats(options?:any){
+    let url = '';
+    if(options){
+      const id = options;
+      url = this.constants.API_ENDPOINT + '/dailystats/' + id;
+    }else{
+      url = this.constants.API_ENDPOINT + '/dailystats';
+    }
+    const response = await lastValueFrom(this.http.get(url));
+    return response as any[];
+  }
+}
