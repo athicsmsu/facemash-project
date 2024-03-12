8ad69fe926ddb53ea0923d63b3c70518a2ebbe0e
athicsmsu <65011212145@msu.ac.th>
2024-03-13 01:33:01 +0700

    new

--- src/app/page/user/edit/edit.component.ts
+++ src/app/page/user/edit/edit.component.ts
@@ -6,20 +6,20 @@ import { UserService } from '../../../services/api/user.service';
 import { ActivatedRoute, Router, RouterLink, RouterOutlet } from '@angular/router';
 import { ToastModule } from 'primeng/toast';
 import { MessageService } from 'primeng/api';
-
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-edit',
   standalone: true,
   imports: [RouterLink, RouterOutlet,ToastModule],
   templateUrl: './edit.component.html',
-  styleUrl: './edit.component.scss'
+  styleUrl: './edit.component.scss',
+  providers: [MessageService]
 })
 export class EditComponent {
   id:any;
   x:any;
-
-  constructor(private messageService: MessageService, private route: ActivatedRoute, private http:HttpClient,private constants : Constants, private router: Router,private header:HeaderComponent,private userService :UserService){
+  constructor(private messageService: MessageService, private route: ActivatedRoute, private http:HttpClient,private constants : Constants, private router: Router,private header:HeaderComponent,private userService :UserService,private toastr: ToastrService){
     this.id = localStorage.getItem('user');
     console.log(this.id);
   }
@@ -28,9 +28,11 @@ export class EditComponent {
     const x = await this.userService.UpdatePassword(this.id,oldPass.value,newPass.value,cfPass.value);
     console.log(x);
   }
-
+  showSuccess() {
+    this.toastr.success('Hello world!', 'Toastr fun!');
+  }
   show() {
     this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Message Content' });
-  }
+}
 
 }
